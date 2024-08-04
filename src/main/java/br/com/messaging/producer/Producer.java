558575666e895f90d75b964b1eeb1031fea24b07
maxjdev <maxjramosdev@gmail.com>
2024-08-04 16:36:10 -0300

    add producer

---
+++ src/main/java/br/com/messaging/producer/Producer.java
@@ -0,0 +1,17 @@
+package br.com.messaging.producer;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Producer {
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public Producer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("hello-topic", message);
+    }
+}
