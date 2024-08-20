773b2705ed32b6b288458e8524a8ef54f1d15686
maxjdev <maxjramosdev@gmail.com>
2024-08-19 21:58:02 -0300

    refactor: replaces direct instantiation with builder

--- src/main/java/br/com/messaging/configs/kafka/topics/KafkaTopicConfig.java
+++ src/main/java/br/com/messaging/configs/kafka/topics/KafkaTopicConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
@@ -26,6 +27,10 @@ public class KafkaTopicConfig {
 
     @Bean
     public NewTopic createTopic() {
-        return new NewTopic(topicName, 1, (short)1);
+        return TopicBuilder
+                .name(topicName)
+                .partitions(1)
+                .replicas(1)
+                .build();
     }
 }
